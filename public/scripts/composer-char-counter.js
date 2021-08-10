b65fb2713acab7f76cdf3a5c51bde70e43bbdf57
Dianne <mdianne.gabriel@gmail.com>
2021-08-09 21:29:30 -0400

    Refactor composer-char-counter. Removed else statement

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
   $(`#tweet-text`).keyup(function() {
+    $(`.counter`).css('color', '#545149');
     const remain = 140 - $(this).val().length;
     $(`.counter`).text(remain);
-
     if (remain < 0) {
       $(`.counter`).css('color', 'red');
-    } else {
-      $(`.counter`).css('color', '#545149');
     }
   });
 });
