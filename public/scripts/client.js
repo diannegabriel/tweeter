ffaf2595c5d1ffcb88fa02b0f97e1b1ac7088c61
Dianne <mdianne.gabriel@gmail.com>
2021-08-10 11:10:11 -0400

    Update client.js with hardcoded code. Set up for DOM

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,3 +3,21 @@
 //  * jQuery is already loaded
 //  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 //  */
+
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+$(document).ready(function() {
+
+});
