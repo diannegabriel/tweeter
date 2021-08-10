83edff2d49bca8cd405ecdb8dda9d9efe5190d98
Dianne <mdianne.gabriel@gmail.com>
2021-08-10 12:54:42 -0400

    Update renderTweets. Change tweetData to an array so it can be iterable

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,7 +5,8 @@
 //  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
+const tweetData = [
+  {
   "user": {
     "name": "Newton",
     "avatars": "https://i.imgur.com/73hZDYK.png",
@@ -15,7 +16,8 @@ const tweetData = {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
   "created_at": 1461116232227
-}
+  }
+]
 console.log(timeago.format(tweetData.created_at));
 
 // $(document).ready(function() {
@@ -51,13 +53,17 @@ const createTweetElement = (tweetData) => {
 
 // This function can be responsible for taking in an array of tweet objects
 // and then appending each one to the #tweets-container
-const renderTweets = function(tweets) {
+const renderTweets = function(tweetData) {
   // loops through tweets
   // for of?
-
+  for (const tweet in tweetData) {
+    $('#tweets-container').append(createTweetElement(tweet));
+  }
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
 }
+
+console.log(renderTweets(tweetData));
 
 // const $tweet = createTweetElement(tweetData);
 // console.log($tweet); // to see what it looks like
