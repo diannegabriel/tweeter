eade971f8fb0ccdb6f0161a1dcd0e380cd55c826
Dianne <mdianne.gabriel@gmail.com>
2021-08-10 15:12:48 -0400

    Add conditionals for validation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -42,6 +42,13 @@ $(document).ready(function() {
       data: $(this).serialize()
     });
   });
+  
+  if ($('.textarea').val() === '') {
+    alert(`There's nothing here! Try again?`);
+  }
+  if ($('.textarea').val().length > 140) {
+    alert(`It's limited to 140 characters, mate! Try again?`);
+  }
 });
 
 
