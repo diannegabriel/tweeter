eac8f24559bd4644cd5534dbd1034817f45e5efb
Dianne <mdianne.gabriel@gmail.com>
2021-08-10 13:37:48 -0400

    Gary helped with some of the problems with the code and fixed/updated the functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,65 +5,52 @@
 //  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = [
+const data = 
+[
   {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   }
 ]
-console.log(timeago.format(tweetData.created_at));
 
-// $(document).ready(function() {
-const createTweetElement = (tweetData) => {
-  const $tweet = $('<article>').addClass('tweetArticle');
-  const html = `
-  <div class="user">
-    <div class="userAvatar">
-      <img src="${tweetData.user.avatars}" class="avatar">
-      ${tweetData.user.name}
-    </div>
-    <div class="handle">
-    ${tweetData.user.handle}
-    </div>
-  </div>
-  <div class="tweeted">${tweetData.content.text}</div>
-  <footer>
-    <div class="date">
-    ${timeago.format(tweetData.created_at)}
-    </div>
-    <div class="engagement">
-      <i class="iconFooter fas fa-flag"></i>
-      <i class="iconFooter fas fa-retweet"></i>
-      <i class="iconFooter fas fa-heart"></i>
-    </div>
-  </footer>
-  `
-  console.log(html)
-  console.log(timeago.format(tweetData.created_at))
-  return $tweet.html(html);
-}
-// });
+// console.log(timeago.format(tweetData.created_at));
+
+$(document).ready(function() {
+// const $tweeting = createTweetElement(tweetData);
+// console.log($tweeting);
+
+renderTweets(data);
+});
 
 // This function can be responsible for taking in an array of tweet objects
 // and then appending each one to the #tweets-container
-const renderTweets = function(tweetData) {
+const renderTweets = function(tweets) {
   // loops through tweets
-  // for of?
-  for (const tweet in tweetData) {
-    $('#tweets-container').append(createTweetElement(tweet));
+
+  for (const tweet of tweets) {
+    $('#tweets-container').prepend(createTweetElement(tweet));
   }
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
 }
-
-console.log(renderTweets(tweetData));
 
 // const $tweet = createTweetElement(tweetData);
 // console.log($tweet); // to see what it looks like
@@ -73,3 +60,32 @@ console.log(renderTweets(tweetData));
 // $(document).ready(function() {
   // const $tweet = $(`<article class="tweet">Hello world</article>`);
 // });
+
+const createTweetElement = (tweet) => {
+  const $tweet = $('<article>').addClass('tweetArticle');
+  const html = `
+  <div class="user">
+    <div class="userAvatar">
+      <img src="${tweet.user.avatars}" class="avatar">
+      ${tweet.user.name}
+    </div>
+    <div class="handle">
+    ${tweet.user.handle}
+    </div>
+  </div>
+  <div class="tweeted">${tweet.content.text}</div>
+  <footer>
+    <div class="date">
+    ${timeago.format(tweet.created_at)}
+    </div>
+    <div class="engagement">
+      <i class="iconFooter fas fa-flag"></i>
+      <i class="iconFooter fas fa-retweet"></i>
+      <i class="iconFooter fas fa-heart"></i>
+    </div>
+  </footer>
+  `
+  console.log(html)
+  console.log(timeago.format(tweet.created_at))
+  return $tweet.html(html);
+}
