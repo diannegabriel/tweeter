5a580c50ad934558644b599464241d0c698cfc8f
Dianne <mdianne.gabriel@gmail.com>
2021-08-11 19:35:42 -0400

    More clean up and adding comments

--- public/scripts/composer-scroll-top.js
+++ public/scripts/composer-scroll-top.js
@@ -1,14 +1,14 @@
 $(document).ready(() => {
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 100) {
-        $('.scroll-up').fadeIn(200);
-      } else {
-        $('.scroll-up').hide();
-      }
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.scroll-up').fadeIn(200);
+    } else {
+      $('.scroll-up').hide();
+    }
     });
     $('.scroll-up').click(
-      function (e) {
-          $('html, body').animate({scrollTop: '0px'}, 800);
+      function () {
+        $('html, body').animate({scrollTop: '0px'}, 800);
       }
   );
 });
