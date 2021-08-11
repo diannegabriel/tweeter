c8a91ca71647333d91123821803ec42b05899320
Dianne <mdianne.gabriel@gmail.com>
2021-08-11 17:16:45 -0400

    Stretch: add scroll behaviour for smooth transition

--- public/scripts/composer-scroll-top.js
+++ public/scripts/composer-scroll-top.js
@@ -6,4 +6,9 @@ $(document).ready(() => {
         $('.scroll-up').hide();
       }
     });
+    $('.scroll-up').click(
+      function (e) {
+          $('html, body').animate({scrollTop: '0px'}, 800);
+      }
+  );
 });
