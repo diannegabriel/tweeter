4124a608ca69d20360350bb21892691510f1a243
Dianne <mdianne.gabriel@gmail.com>
2021-08-11 17:01:51 -0400

    Stretch: play around with scroll-up

--- public/scripts/composer-scroll-top.js
+++ public/scripts/composer-scroll-top.js
@@ -2,6 +2,12 @@ $(document).ready(() => {
   $(window).scroll(function() {
     $('.scroll-up').fadeIn(200);
     $('.scroll-up').show();
+    // if ($(window).scrollTop(0)) {
+    //   $('.scroll-up').fadeOut(200);
+    // }
+    // $(window).scrollTop(0);
+
+
 
   });
 });
