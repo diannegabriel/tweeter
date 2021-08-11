9853647f903a8d29ee9d95337f2e10202baf23a1
Dianne <mdianne.gabriel@gmail.com>
2021-08-11 17:15:50 -0400

    Stretch: fix scroll-up

--- public/scripts/composer-scroll-top.js
+++ public/scripts/composer-scroll-top.js
@@ -1,16 +1,9 @@
 $(document).ready(() => {
-  // if ($(this).scrollTop() === 0) {
     $(window).scroll(function() {
-      // $('.scroll-up').fadeIn(200);
-      $('.scroll-up').show();
-      // if ($(window).scrollTop(0)) {
-        //   $('.scroll-up').fadeOut(200);
-        // }
-      });
-    // }
-    // $(window).scrollTop(0);
-    $('.scroll-up').click(() => {
-      $('.scroll-up').fadeOut(200);
-      // $('.scroll-up').hide();
+      if ($(this).scrollTop() > 100) {
+        $('.scroll-up').fadeIn(200);
+      } else {
+        $('.scroll-up').hide();
+      }
     });
 });
