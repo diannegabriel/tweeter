355fcc617fa3529373311dd31b4033d6617a73d8
Dianne <mdianne.gabriel@gmail.com>
2021-08-12 12:53:34 -0400

    Eslint composer-char-counter.js. Fix formatting. Add comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,21 @@
+// ============================ DOM IS READY =============================
+
 $(document).ready(function() {
+
+  // When the user types in the #tweet-text textarea,
+  // .counter will count the remaining characters left.
   $(`#tweet-text`).keyup(function() {
     $(`.counter`).css('color', '#545149');
     const remain = 140 - $(this).val().length;
     const $error = $('#error');
     $(`.counter`).text(remain);
+   
+    // If there's an error present, the error will slide up
+    // when .counter is not empty or less than 140 characters.
     if (remain >= 0 && remain < 140) {
       $error.slideUp();
     }
+    // .counter will turn red as soon as it hits below 0.
     if (remain < 0) {
       $(`.counter`).css('color', 'red');
     }
